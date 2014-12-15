1ee1fcb122190428300f019420c1c0a3df25b11a
rabi <rabi.rabi.shaw@gmail.com>
2014-12-15 08:28:47 +0000

    modified

--- 1.c
+++ 1.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main (void )
 {
-printf ( "\n rabi ");
-return ( 0 );
+    int a;
+    printf ( "\n enter the value of a \n ");
+    scanf ( "%d", &a );
+    printf ( "\n entered value is %d ",a);
+    return ( 0 );
 }
+
+
